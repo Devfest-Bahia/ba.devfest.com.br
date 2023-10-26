e424132c21f24ad1f333d76ac91476273d43bbb5
Antonio Pedro Ferreira <apsf88@gmail.com>
2023-10-26 01:35:38 -0300

    adjust wrong text desfest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'desfestba'`, () => {
+  it(`should have as title 'devfestba'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('desfestba');
+    expect(app.title).toEqual('devfestba');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('desfestba app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('devfestba app is running!');
   });
 });
