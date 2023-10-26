1c67cdb2810eeb292cb363ac70468c726424f93e
Antonio Pedro Ferreira <apsf88@gmail.com>
2023-10-26 04:07:18 -0300

    create a stable verison

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContadorComponent } from './contador/contador.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EquipeComponent } from './equipe/equipe.component';
 import { PatrocinadoresComponent } from './patrocinadores/patrocinadores.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { InscricaoComponent } from './inscricao/inscricao.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/sobre', pathMatch:'full'},
-  {path: 'ingressos', component:ContadorComponent},
+  {path: 'ingressos', component:InscricaoComponent},
   {path: 'palestrantes', component:PalestrantesComponent},
   {path: 'equipe', component:EquipeComponent},
-  {path: 'patrocinadores', component:PatrocinadoresComponent},
-
+  {path: 'patrocine', component:PatrocinadoresComponent},
 ];
 
 @NgModule({
