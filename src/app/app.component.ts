932ec006179367940cd70c9914bf173cf36192ff
Igor <igorjbahia@gmai.com>
2023-10-29 17:59:56 -0300

    :sparkles: feat: Uncommented and implemented previously commented functionalities, refactored project code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devfestba';
+  bannerImage: string;
+
+  constructor(private router: Router) {
+    this.bannerImage = '../assets/DF23-Logo-2650-Red.svg';
+    this.updateBanner();
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+  navigateToExternalUrl(url: string) {
+    window.open(url, '_blank');
+  }
+
+  updateBanner() {
+    const width = window.innerWidth;
+    if (width >= 1582) {
+      this.bannerImage = '../assets/DF23-Logo-2650-Red.svg';
+    } else if (width <= 640) {
+      this.bannerImage = '../assets/DF23-Logo-640-Red.svg';
+    } else {
+      this.bannerImage = '../assets/DF23-Logo-1440-Red.svg';
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateBanner();
+  }
 }
+
